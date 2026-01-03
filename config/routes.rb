f308b5b092e22d1ac5b4814d5cad9ca386fe0cdf
Thomas Minetos <athanasios.minetos@gmail.com>
2026-01-03 09:05:14 -0600

    Added CLI webhook

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
     
     get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
+
+    post '/charges/mark_complete' => 'charges#mark_complete'
+    
   end
 end
